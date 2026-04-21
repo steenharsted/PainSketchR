09bb0bd10bc9fe32ddf6ad1bcbcd0157ebd70493
Soren ONeill <soren@oneill.dk>
2026-04-21 09:27:22 +0200

    Completed documentation for the pd_anatomy_merge function. Also renamed pd_recreate file to pd_recreate_drawing

--- R/pd_anatomy_merge.R
+++ R/pd_anatomy_merge.R
@@ -1,32 +1,51 @@
-#' Merge anatomical areas into larger areas
+#' Merge connected and overlapping anatomical regions
 #'
-#' This function takes as input a pain drawing data structure of anatomical regions, 
-#' where each row/paindrawing is a single anatomical area defined by one stroke/polygon.
+#' This function merges all connected or overlapping anatomical regions into one region. Any
+#' unconnected or none-overlapping regions are retained as separate regions.
 #' 
-#' The function collapses these n pain drawings of each 1 polygon into less-than n pain drawing of n polygons. 
+#' This function takes as input a pain drawing data structure, where each row (pain drawing)
+#' represents an anatomical region, and each anatomical region is defined by a single 
+#' stroke/polygon.
 #' 
-#' The resulting number of pain drawings is dependent upon the overlaps between pain drawings in the `pd`:
+#' The function merges those anatomcial regions (represented in pd as pain drawings) which 
+#' are connected or overlap.
 #' 
-#' * Any connected or overlapping paindrawings will be merged. 
-#' * Any discrete (groups of) paindrawings will be returned as separate pain drawings.
+#' * Any connected or overlapping anatomical areas will be merged. 
+#' * Any discrete (groups of) unconnected and not-overlapping anatomical areas will be returned 
+#' as separate resulting pain drawings (rows).
 #' 
-#' Thus if `pd` consists of three pain drawings (rows) 'A', 'B' and 'C', where only 'A' and 'B' overlap, the resulting pain drawing data structure
-#' will consist of two pain drawings (rows) 'A' (the resulting merger of 'A' and 'B') and 'C'. Note, that mergers will retain all other variables 
-#' (columns) from the first pain drawing in that subset of rows.
+#' Thus if `pd` consists of three pain drawings (rows) with id = 'A', 'B' and 'C', where only 
+#' 'A' and 'B' overlap, the resulting pain drawing data structure will consist of two pain 
+#' drawings (rows): 'A' (the resulting merger of 'A' and 'B') and 'C'. Note, that mergers will 
+#' retain all other variables (columns) from the first pain drawing in that subset of rows, e.g. 
+#' the `id` 'A'.
 #' 
-#' This is useful for creating anatomical templates -- e.g. the anterior aspect of a full body template may consist of n different anatomical areas
-#' which all border or overlap each other. The function will collapse these into a single pain drawing of n polygons relying on the function
-#' `pd_poly_manage_overlap` function. This new pain drawing with a single polygon (anterior aspect of the body outline) can be used to define a new 
-#' anatomical region. Similarly, a pain drawing data structure with discrete groups of anatomical areas of both the anterior and posterior aspects 
-#' will return separate merged areas for the anterior and posterior regions.
+#' This function is useful for creating anatomical templates -- e.g. the anterior aspect of a 
+#' full body template may consist of n different anatomical areas which all border or overlap 
+#' each other. The function can be used to collapse these into a single pain drawing of n 
+#' polygons, which are then merged. 
+#' The resulting pain drawing structure with a single polygon (anterior aspect of the body 
+#' outline) can be used to define a new anatomical region for analyses. 
+#' Similarly, a pain drawing data structure with discrete groups of anatomical areas such as
+#' e.g. both the anterior and posterior aspects will return separate regions where subregions
+#' are merged within the anterior and posterior regions.
 #' 
-#' @param `pd` must be a valid pain drawing data structure -- use `pd_check_data()` to confirm the data structure. Furthermore, each row in `pd` 
+#' @param pd must be a valid pain drawing data structure. Furthermore, each row in `pd` 
 #' should consist of a pain drawing with just a single stroke/polygon.
-#' @returns A new pain drawing data structure where the column `p` has been collapsed into a merged pain area -- all other columns retain values from 
-#' the first pain drawing of that merger.
+#' @returns A new pain drawing data structure where the column `p` has been collapsed into a 
+#' merged pain area -- all other columns retain values from the first pain drawing of that merger.
 #'
 #' @export
 #' @examples
+#' pd_anatomy_merge(pd_demo_anatomy)
+#' 
+#' bi <- system.file("extdata", "mird_body_background.png", package = "paindrawings")
+#' pd_demo_anatomy |> pd_recreate_drawing(background_image = bi)
+#' pd_demo_anatomy |> pd_anatomy_merge() |> pd_recreate_drawing(background_image = bi)
+#' 
+#' pd_demo_anatomy |> dplyr::filter(id %in% c("Front_left_thigh", "Front_left_leg", "Front_left_foot")) |> pd_recreate_drawing(background_image = bi)#' 
+#' pd_demo_anatomy |> dplyr::filter(id %in% c("Front_left_thigh", "Front_left_leg", "Front_left_foot")) |> pd_anatomy_merge() |> pd_recreate_drawing(background_image = bi)
+#' 
 pd_anatomy_merge <- function(pd) {
   if (!pd_check_data(pd, verbose=FALSE)) { stop("`pd` must be a valid pain drawing data structure")}
 
@@ -58,7 +77,7 @@ pd_anatomy_merge <- function(pd) {
     dplyr::group_by(i) |> 
     dplyr::group_split() |>
     purrr::map(\(tbl) {
-      tbl$i <- 1
+      tbl$i <- as.integer(1)
       tbl
     })
   # Just latch `p` onto `result`
