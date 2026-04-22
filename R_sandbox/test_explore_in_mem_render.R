e7f564479557d5cdf5efd9cba68afbdc196ffb30
Steen <steenharsted@gmail.com>
2026-04-22 15:13:09 +0200

    benchmarking continued

--- R_sandbox/test_explore_in_mem_render.R
+++ R_sandbox/test_explore_in_mem_render.R
@@ -2,59 +2,59 @@
 # Exploring in memory render. We should be able to do this using unigd::ugd_render()
 # First we need to be able to 100% recreate the ggsave output using png() og ugd_save
 
-library(ggplot2)
-a <- penguins |>
-  ggplot(aes(x = bill_len, y = bill_dep, color = species)) +
-  geom_point()
+# library(ggplot2)
+# a <- penguins |>
+#   ggplot(aes(x = bill_len, y = bill_dep, color = species)) +
+#   geom_point()
 
-# Save with base
-png("test_base.png", width = 600, height = 400, units = "px")
-print(a)
-dev.off()
+# # Save with base
+# png("test_base.png", width = 600, height = 400, units = "px")
+# print(a)
+# dev.off()
 
-# Save with ggsave
-ggsave("test_ggsave.png", a, width = 600, height = 400, units = "px")
+# # Save with ggsave
+# ggsave("test_ggsave.png", a, width = 600, height = 400, units = "px")
 
+# # WHY ARE THE OUTPUTS SO DIFFERENT?
 
-# WHY ARE THE OUTPUTS SO DIFFERENT?
+# ####
+# # ggsave default: 300 dpi, so 600px / 300dpi = 2 inches wide
+# png("test_base_match.png", width = 600, height = 400, units = "px", res = 300)
+# print(a)
+# dev.off()
 
-####
-# ggsave default: 300 dpi, so 600px / 300dpi = 2 inches wide
-png("test_base_match.png", width = 600, height = 400, units = "px", res = 300)
-print(a)
-dev.off()
+# ragg::agg_png(
+#   "test_ragg_match.png",
+#   width = 600 / 300, # inches
+#   height = 400 / 300, # inches
+#   res = 300,
+#   units = "in"
+# )
+# a
+# dev.off()
 
+# # in memeory render
 
-ragg::agg_png(
-  "test_ragg_match.png",
-  width = 600 / 300, # inches
-  height = 400 / 300, # inches
-  res = 300,
-  units = "in"
-)
-a
-dev.off()
+# arr <- ragg::agg_capture(width = 600, height = 400, units = "px", res = 300)
+# a
+# cap <- arr() # chr matrix of hex/named colours
+# dev.off()
 
+# # Convert colour names → integer RGBA → numeric [0,1] array
+# rgba_int <- col2rgb(cap, alpha = TRUE) # 4 × (600*400) matrix, values 0-255
+# rgba_arr <- array(
+#   t(rgba_int) / 255, # normalise to [0,1]
+#   dim = c(400, 600, 4) # height × width × RGBA
+# )
 
-# in memeory render
+# rgba_arr
 
-arr <- ragg::agg_capture(width = 600, height = 400, units = "px", res = 300)
-a
-cap <- arr() # chr matrix of hex/named colours
-dev.off()
+# grid::grid.newpage()
+# rgba_arr |> grid::grid.raster()
 
-# Convert colour names → integer RGBA → numeric [0,1] array
-rgba_int <- col2rgb(cap, alpha = TRUE) # 4 × (600*400) matrix, values 0-255
-rgba_arr <- array(
-  t(rgba_int) / 255, # normalise to [0,1]
-  dim = c(400, 600, 4) # height × width × RGBA
-)
-
-rgba_arr
-
-grid::grid.newpage()
-rgba_arr |> grid::grid.raster()
-
+library(tidyverse)
+library(devtools)
+load_all()
 
 # data
 pd <- pd_import_json(c(
@@ -142,7 +142,7 @@ pd |>
 
 
 ### Benchmark
-bench::mark(  
+bench::mark(
   file_based = {
     set.seed(1)
     pd |> pd_recreate_drawing(background_image = background_image)
@@ -163,3 +163,43 @@ bench::mark(
 # 1 file_based 741.32ms 741.32ms     1.35         NA    12.1      1     9   741.32ms <NULL> <NULL>
 # 2 in_memory     1.54s    1.58s     0.633        NA     2.53     2     8      3.16s <NULL> <NULL>
 # # ℹ 2 more variables: time <list>, gc <list>
+
+#### LETS BENCHMARK pd_to_png() file vs mem
+## SIGNLE ROWS
+
+bench::mark(
+  file_based = {
+    set.seed(1)
+    pd_to_png_single(pd[3, ])
+  },
+  in_memory = {
+    set.seed(1)
+    pd_to_png_single_in_mem(pd[3, ])
+  },
+  iterations = 20,
+  check = FALSE,
+  memory = FALSE
+)
+
+# Output of Linux machines 2026_04_22
+# A tibble: 2 × 13
+#   expression      min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc total_time result memory time            gc
+#   <bch:expr> <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>   <bch:tm> <list> <list> <list>          <list>
+# 1 file_based   54.3ms   55.8ms      17.1        NA     14.0    11     9      642ms <NULL> <NULL> <bench_tm [20]> <tibble [20 × 3]>
+# 2 in_memory    57.4ms   58.8ms      16.8        NA     16.8    10    10      596ms <NULL> <NULL> <bench_tm [20]> <tibble [20 × 3]>
+
+bench::mark(
+  file_based = {
+    set.seed(1)
+    pd |> pd_to_png()
+  },
+  in_memory = {
+    set.seed(1)
+    pd_to_png_single_in_mem(pd[3, ])
+  },
+  iterations = 20,
+  check = FALSE,
+  memory = FALSE
+)
+
+pd |> pd_to_png()
