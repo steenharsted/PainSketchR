141614c3e5f0afad75c1a7f77b4e431f524b26d7
Soren ONeill <soren@oneill.dk>
2026-05-08 12:18:59 +0200

    Completed the implode function

--- R/pdr_implode.R
+++ R/pdr_implode.R
@@ -1,39 +1,22 @@
-#' Merge connected and overlapping anatomical regions
+#' Implode several pain drawwings into one.
 #'
-#' This function merges all connected or overlapping anatomical regions into one region. Any
-#' unconnected or none-overlapping regions are retained as separate regions.
-#' 
 #' This function takes as input a pain drawing data structure, where each row (pain drawing)
-#' represents an anatomical region, and each anatomical region is defined by a single 
-#' stroke/polygon.
+#' represents e.g. an anatomical region. The function merges these into a single pain drawing
+#' (row) with multiple strokes/polygons.
 #' 
-#' The function merges those anatomcial regions (represented in pd as pain drawings) which 
-#' are connected or overlap.
-#' 
-#' * Any connected or overlapping anatomical areas will be merged. 
-#' * Any discrete (groups of) unconnected and not-overlapping anatomical areas will be returned 
-#' as separate resulting pain drawings (rows).
-#' 
-#' Thus if `pd` consists of three pain drawings (rows) with id = 'A', 'B' and 'C', where only 
-#' 'A' and 'B' overlap, the resulting pain drawing data structure will consist of two pain 
-#' drawings (rows): 'A' (the resulting merger of 'A' and 'B') and 'C'. Note, that mergers will 
-#' retain all other variables (columns) from the first pain drawing in that subset of rows, e.g. 
-#' the `id` 'A'.
-#' 
-#' This function is useful for creating anatomical templates -- e.g. the anterior aspect of a 
-#' full body template may consist of n different anatomical areas which all border or overlap 
-#' each other. The function can be used to collapse these into a single pain drawing of n 
-#' polygons, which are then merged. 
-#' The resulting pain drawing structure with a single polygon (anterior aspect of the body 
-#' outline) can be used to define a new anatomical region for analyses. 
-#' Similarly, a pain drawing data structure with discrete groups of anatomical areas such as
-#' e.g. both the anterior and posterior aspects will return separate regions where subregions
-#' are merged within the anterior and posterior regions.
+#' The function is useful for merging anatomcial regions (represented in pd as pain drawings) 
+#' which are connected or overlap.
 #' 
 #' @param pd must be a valid pain drawing data structure. Furthermore, each row in `pd` 
-#' should consist of a pain drawing with just a single stroke/polygon.
-#' @returns A new pain drawing data structure where the column `p` has been collapsed into a 
-#' merged pain area -- all other columns retain values from the first pain drawing of that merger.
+#' should consist of a pain drawing with just a single stroke/polygon -- if not, the pain
+#' drawing will be exploded into multiple paindrawings before imploding.
+#' @returns A new pain drawing data structure where the column `p` has been imploded into one 
+#' set of coordinates, of multiple strokes. THe `s` columns retains all information from the
+#' original strokes (in multiple pain drawings), but all other columns are replaced so the
+#' result contains a single row: `w` and `h` are replaced by the maximum value observed, 
+#' id is placed with 'imploded', `coord` and `app` are retained or replace with 'multiple' if
+#' muliple values are observed, `ts` is replaced by the maximum (latest) value observed. All
+#' other columns (if present) are deleted.
 #'
 #' @export
 #' @examples
@@ -66,23 +49,27 @@ pdr_implode <- function(pd) {
   # We can thus collapse this into a pd data structure with just one pain drawing of
   # multiple strokes/polygons, numbered (i) 1:n, where n is nrow(pd).
 
-  s <- pd |> dplyr::select(s) |> tidyr::unnest(cols=s)
-  p <- pd |> dplyr::select(p) |> tidyr::unnest(cols=p)
+  # s <- pd |> dplyr::select(s) |> tidyr::unnest(cols=s)
+  # p <- pd |> dplyr::select(p) |> tidyr::unnest(cols=p)
   # Retain all other columns in `pd` but `p`...
-  all_but_p_and_s <- pd |> dplyr::select(-p, -s)
+  all_but_p_and_s <- pd |> dplyr::select(-p, -s) |>
+    dplyr::summarize(
+      id = "imploded",
+      w = max(w),
+      h = max(h),
+      coord = ifelse(length(unique(coord))>1, "multiple", coord[1]),
+      ts = max(ts), 
+      app = ifelse(length(unique(app))>1, "multiple", app[1])
+    )
   # Create a new tibble ... pd collapsed into just column p with only one element - a tibble with a unique 'i' for each pain drawing in pd
   just_p_collapsed <- tibble::tibble(p = pd |> dplyr::pull(p) |> purrr::list_rbind(names_to = 'i') |> list() )
   just_s_collapsed <- tibble::tibble(s = pd |> dplyr::pull(s) |> purrr::list_rbind(names_to = 'i') |> list() )
 
-  # just_p <- 
-  #   just_p_collapsed$p[[1]] |> 
-  #   dplyr::group_by(i) |> 
-  #   dplyr::group_split() |>
-  #   purrr::map(\(tbl) {
-  #     tbl$i <- as.integer(1)
-  #     tbl
-  #   })
-  # # Just latch `p` onto `result`
-  # result$p <- just_p
-  # return(result) 
+  return(
+    dplyr::bind_cols(
+      all_but_p_and_s,
+      just_s_collapsed,
+      just_p_collapsed
+    )
+  )
 }
